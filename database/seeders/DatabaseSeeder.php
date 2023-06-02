abc6d89c066cf8dd49babde2c432142047e0e426
Paul Arnold Simbajon <40814631+pasimbajon@users.noreply.github.com>
2023-06-02 17:10:19 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call([
+            GameGenre::class,
+            GameSeeder::class,
+            UserSeeder::class
+        ]);
     }
 }
