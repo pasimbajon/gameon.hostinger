a7df9fdd7192efd14b6975a0f5f6beed9dfa5850
Paul Arnold Simbajon <40814631+pasimbajon@users.noreply.github.com>
2023-06-02 17:16:31 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\GameController;
 
 /*
 |--------------------------------------------------------------------------
